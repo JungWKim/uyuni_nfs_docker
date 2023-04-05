06aacef14137d04f0fcf0a655d37c54a3c638560
JungWKim <lionmini@naver.com>
2023-04-05 21:23:26 +0900

    .

--- setup_master.sh
+++ setup_master.sh
@@ -80,11 +80,19 @@ EOF
 
 sudo sysctl --system
 
-# download docker gpg key
-#sudo apt update
-#sudo apt install -y ca-certificates curl gnupg lsb-release
-#sudo mkdir -m 0755 -p /etc/apt/keyrings
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# install docker
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# login docker account
+sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PW}
 
 # ssh configuration
 ssh-keygen -t rsa
@@ -125,9 +133,6 @@ echo "source <(kubeadm completion bash)" >> ${HOME}/.bashrc
 
 echo "source <(kubectl completion bash)" | sudo tee -a /root/.bashrc
 echo "source <(kubeadm completion bash)" | sudo tee -a /root/.bashrc
-
-# login docker account
-sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PW}
 
 # install nvidia-container-toolkit
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
