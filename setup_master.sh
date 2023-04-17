2347160bace65043e06987dbb76188aee8763f97
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2023-04-17 21:39:08 +0900

    Update setup_master.sh

--- setup_master.sh
+++ setup_master.sh
@@ -75,6 +75,13 @@ sed -i "s/container_manager: containerd/container_manager: docker/g" inventory/m
 sed -i "s/docker_containerd_version: 1.4.12/docker_containerd_version: latest/g" roles/download/defaults/main.yml
 sed -i "s/host_architecture }}]/host_architecture }} signed-by=\/etc\/apt\/keyrings\/docker.gpg]/g" roles/container-engine/docker/vars/ubuntu.yml
 
+# enable kubectl & kubeadm auto-completion
+echo "source <(kubectl completion bash)" >> ${HOME}/.bashrc
+echo "source <(kubeadm completion bash)" >> ${HOME}/.bashrc
+
+echo "source <(kubectl completion bash)" | sudo tee -a /root/.bashrc
+echo "source <(kubeadm completion bash)" | sudo tee -a /root/.bashrc
+
 # automatically disable swap partition
 ansible-playbook -i inventory/mycluster/hosts.yaml  --become --become-user=root cluster.yml -K
 sleep 120
@@ -84,13 +91,6 @@ cd ~
 mkdir -p ${HOME}/.kube
 sudo cp -i /etc/kubernetes/admin.conf ${HOME}/.kube/config
 sudo chown ${USER}:${USER} ${HOME}/.kube/config
-
-# enable kubectl & kubeadm auto-completion
-echo "source <(kubectl completion bash)" >> ${HOME}/.bashrc
-echo "source <(kubeadm completion bash)" >> ${HOME}/.bashrc
-
-echo "source <(kubectl completion bash)" | sudo tee -a /root/.bashrc
-echo "source <(kubeadm completion bash)" | sudo tee -a /root/.bashrc
 
 # install helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
