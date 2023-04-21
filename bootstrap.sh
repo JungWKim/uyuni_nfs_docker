b5a1346d90e5f443d6c6c8c03627f969facd10b7
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2023-04-22 01:47:00 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -74,6 +74,10 @@ sed -i "s/docker_version: '20.10'/docker_version: 'latest'/g" roles/container-en
 sed -i "s/container_manager: containerd/container_manager: docker/g" inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
 sed -i "s/docker_containerd_version: 1.4.12/docker_containerd_version: latest/g" roles/download/defaults/main.yml
 sed -i "s/host_architecture }}]/host_architecture }} signed-by=\/etc\/apt\/keyrings\/docker.gpg]/g" roles/container-engine/docker/vars/ubuntu.yml
+sed -i "s/# cri_dockerd_enabled: false/cri_dockerd_enabled: false/g" inventory/mycluster/group_vars/all/docker.yml
+sed -i "s/container_manager: containerd/container_manager: docker/g" roles/kubespray-defaults/defaults/main.yaml
+sed -i "s/# container_manager: containerd/container_manager: docker/g" inventory/mycluster/group_vars/all/etcd.yml
+sed -i "s/etcd_deployment_type: containerd/etcd_deployment_type: docker/g" inventory/mycluster/group_vars/all/etcd.yml
 
 # enable kubectl & kubeadm auto-completion
 echo "source <(kubectl completion bash)" >> ${HOME}/.bashrc
